1c2f48d1d023196e46ca1f2ad6745e3bfa041a67
Drumir <drumir@mail.ru>
2021-02-28 10:08:53 +0300

    v0.4 Исправлена работа с RTC

--- main.c
+++ main.c
@@ -21,6 +21,8 @@
 char str[10];
 uint8_t ch2 = 0b01010101;
 uint8_t Sec, Min_1, Min_10, Hours_1, Hours_10;
+uint8_t hour, min, sec;
+uint16_t sqw;
 
 int main(void)
 {
@@ -29,26 +31,24 @@ int main(void)
 	
 	DDRD = 0b00000000;			// ,,,,,,1s int, 
 	
-	MCUCR |= (1<<ISC01) | (1<<ISC00); // The rising edge of INT0 generates an interrupt request
+	EICRA |= (1<<ISC01) | (1<<ISC00); // The rising edge of INT0 generates an interrupt request
+
 	EIMSK |= (1<<INT0); // External Interrupt Request 0 Enable
 
-	//rtc_write(0x02, 0x20);		// Установка часов
-	//rtc_write(0x01, 0b01010001); // Установка минут
-	//Sec = rtc_read(0x00);
-	rtc_write(0x0E, 0b01000000);				// Запуск меандра 1 Гц
-	/*if(Sec & 0b10000000)              // Запуск часов
-	{
-		rtc_write(0x0E, 0b01000000);				//
-		rtc_write(0, Sec & 0b01111111);	//
-	}
-*/
+	//rtc_write(0x00, 0b00100010);				// Установка секунд
+	//rtc_write(0x01, 0b00000010);				// Установка минут
+	//rtc_write(0x02, 0b00000000);				// Установка часов
+	//rtc_write(0x0E, 0b01000000);				// Запуск меандра 1 Гц
 
   lcd_init(LCD_DISP_ON);    // init lcd and turn on
 	
 	lcd_gotoxy(0, 0);
-	lcd_putsB(" 10.2M ");
+	lcd_putsB(" 4.21m ");
 	lcd_gotoxy(0, 3);
-	lcd_putsB("  1:40 ");
+	lcd_putsB("  0:41 ");
+  
+  //rtc_get_time(&hour, &min, &sec);
+  
 	sei();
 	
   /* Replace with your application code */
@@ -70,23 +70,34 @@ int main(void)
 //1:45
 ISR(INT0_vect) // 
 {
-	uint8_t t, Sec_1, Sec_10;
+  uint8_t t = rtc_read(0x02);
+  hour = t&0b00001111;
+  hour += 10*t&0b00010000;
+  hour += 20*t&0b00100000;
+  
+  t = rtc_read(0x01);
+  min = t&0b00001111;
+  min += 10*(t>>4);
+  
+  t = rtc_read(0x00);
+  sec = t&0b00001111;
+  sec += 10*(t>>4);
+  
+  t = rtc_read(0x11); // Чтение температуры RTC
+  
+  sqw ++; if(sqw > 99) sqw = 0;
 
-	t = rtc_read(0x00); Sec_1 = t & 0x0F;   Sec_10 = t >> 4;
-	t = rtc_read(0x01); Min_1 = t & 0x0F;   Min_10 = t >> 4;
-	t = rtc_read(0x02); Hours_1 = t & 0x0F; Hours_10 = t >> 4;
-	
 	//sei();              // 
 	lcd_charMode(DOUBLESIZE);
 	lcd_gotoxy(0, 6);
-	lcd_putc('0' + Hours_10);
-	lcd_putc('0' + Hours_1);
-	lcd_putc(':');
-	lcd_putc('0' + Min_10);
-	lcd_putc('0' + Min_1);
+	lcd_putc('0' + hour/10);
+	lcd_putc('0' + hour%10);
+  lcd_putc(':');
+	lcd_putc('0' + min/10);
+	lcd_putc('0' + min%10);
 	lcd_putc(' ');
-	lcd_putc('1');
-	lcd_putc('4');
+	lcd_putc('0' + t/10);
+	lcd_putc('0' + t%10);
 	lcd_putc('C');
 	return;
 }
